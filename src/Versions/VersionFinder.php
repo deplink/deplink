40e7eabc8726fd9aa3e5f6136a80fe4294eb253e
Wojciech Mleczek <mleczek.wojtek@gmail.com>
2017-07-01 19:11:26 +0200

    Version finder interface

---
+++ src/Versions/VersionFinder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Deplink\Versions;
+
+/**
+ * Resolve available package versions for given package.
+ */
+interface VersionFinder
+{
+    /**
+     * @param string $version
+     * @return bool
+     */
+    public function has($version);
+
+    /**
+     * Get all available package versions.
+     *
+     * @return string[]
+     */
+    public function get();
+
+    /**
+     * Get versions which satisfy constraint.
+     *
+     * @param string $constraint Version constraint (eq. ">= 5.3").
+     * @return string[]
+     */
+    public function getSatisfiedBy($constraint);
+
+    /**
+     * @param string $version
+     * @return string[]
+     */
+    public function getGreaterThan($version);
+}
